c166d2a7038af00d6db9c13905cc02983ade80fb
Dinakar <dinakargaddam23@gmail.com>
2026-05-30 11:16:33 +0530

    roboshop-2 from scratch MOngodb

--- roboshop.sh
+++ roboshop.sh
@@ -15,7 +15,7 @@ do
     --query 'Instances[0].InstanceId' \
     --output text)
     echo "Instance ID : $INSTANCE_ID"
-    if [ "$instance" == "frontend"]; then
+    if [ "$instance" == "frontend" ]; then
         IP=$(aws ec2 describe-instances     --instance-ids $INSTANCE_ID \
         --query 'Reservations[*].Instances[*].[ PublicIpAddress]' \
         --output text)
